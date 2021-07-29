3620036bc14ad9227d579f046350a5735c0eb4cb
Jerin Raisa <jerin.raisa11@gmail.com>
2021-07-29 03:17:40 -0400

    first commit

---
+++ copy/content.js
@@ -0,0 +1,13 @@
+const copy = {
+    description: {
+        title: "Hello, welcome to my <em>tiny corner</em> in this big world",
+        description1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        description2: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    },
+    nextSection: {
+        title: "Check out my <em>Experience</em> ➮",
+        link: "https://google.ca"
+    }
+}
+
+export default copy;
